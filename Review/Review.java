a829a2f3ecba77bdb3745a36d407204adde3eb71
797067 <54371478+797067@users.noreply.github.com>
2019-09-17 09:34:05 -0700

    commit

--- Review/Review.java
+++ Review/Review.java
@@ -28,5 +28,83 @@ public class Review
 // Pumpkin p = new Pumpkin(3);
 
 //PROB 3 Write a class with instance variables, constructor, and methods
+// public class Numbers{
+// private int x;
+// private int y;
+// public Numbers (int a, int b){
+// x= a;
+// y= b;}
+// public int multiply (int a, int b){
+// int product = a+b;
+//}
+//}
+
+//PROB 4 Use integer division and modular division to manipulate the digits of an integer
+// EX1
+//public static void main(){
+// int count = 0
+// int num = 12345; 
+// while (num!= 0) {
+// num = num/10;
+// count ++;}
+// system.out.print("Count = " + count);
+//}
+// EX2
+//public static void main(){
+//int sum = 0;
+// int num = 12345;
+// while (num != 0){
+// sum = sum+num%10;
+//num = num%10}
+//system.out.println("sum= " + sum);}
+
+//PROB 5 Create a sum method that will return the sum of the digits in an integer of one or more digits
+// private int x;
+// private int y;
+// public int sum(int a , int b){
+// return a+b;}
+
+// PROB 6 Create simple calc methods such as add, multiply, divide, and subrtact that will take in two integers and return the appropriate value
+// private int x;
+// private int y;
+// public int add(int a, int b){
+// return a+b;}
+// public int subtract(int a, int b){
+// return a-b;}
+// public int multiply(int a, int b){
+// return a*b;}
+// public int divide(int a, int b){
+// return a/b;}
+
+// PROB 7 Show output values for a while loop
+// int cntrl = 0;
+// int num = 5;
+// while (cntrl < num){
+// cntrl ++; 
+// system.out.println ("Num = " + num + "Cntrl = " + cntrl); 
+
+//PROB 8 Understand if and if-else statments.  What will the following print to the screen
+// it would print 
+// Far out
+
+//PROB 9   Create (declare and initialize) an Arrray of random integers named nums.  Use a for loop to initialize the array.
+// int[] nums = new int [10];
+//for (int i=0; i<nums.length; i++){
+// sytem.out.print((int)math.random()*10));
+// system.out.println("nums[ " + i + " ] = " +nums [i]);}
+
+//PROB 10 Use a for-loop to traverse and array
+// public static void main(String[] args) {
+// String[] arrData = {"Alpha", "Beta", "Gamma", "Delta", "Sigma"};    
+// for(int i=0; i< arrData.length; i++){
+// System.out.println(arrData[i]);  }
+
+//PROB 11 Use a for-loop to find the sum of values in an array
+//int sum = 0;
+//for (int i = 0; i < nums.length; i++){
+//sum+= num[i];}
+//system.out.print("sum = " + sum);
+ 
+// PROB 12 Use a for-loop to find the sum of values in an array
 
 }
