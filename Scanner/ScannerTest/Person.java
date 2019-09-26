a337a8a430b7c482b2e807886d69030c0ca94399
Kayka Garcia <kgarcia@WS-ACADBUS.EDU>
2019-09-26 10:44:55 -0700

    commit

--- Scanner/ScannerTest/Person.java
+++ Scanner/ScannerTest/Person.java
@@ -11,44 +11,42 @@ public class Person
     private String mName = " ";
     private String lName = " ";
     private String fullName = " ";
-String inputStr = "";
-        int s = inputStr.indexOf(" ");
-    public Person(String fullName){
-         this.fullName=fullName;
-        
-    }
     
-    public String parseName(){
-         
-        while(inputStr.indexOf(",")!=-1  ){ //1 or 2
-            if(inputStr.indexOf(" ")!= inputStr.lastIndexOf(" ")){
-                lName = inputStr.substring(0,s);        
-                fName = inputStr.substring (s+1,inputStr.lastIndexOf(" "));
-                mName = inputStr.substring(inputStr.lastIndexOf(" ")+1);
+    public Person(String fullName){
+        this.fullName=fullName;
+        parseName();
+    }
+
+    public void parseName(){
+        int s = fullName.indexOf(" ");
+        while(fullName.indexOf(",")!=-1  ){ //1 or 2
+            if(fullName.indexOf(" ")!= fullName.lastIndexOf(" ")){
+                lName = fullName.substring(0,s);        
+                fName = fullName.substring (s+1,fullName.lastIndexOf(" "));
+                mName = fullName.substring(fullName.lastIndexOf(" ")+1);
                 String fullName = lName + fName + mName;
             }else{
-                lName = inputStr.substring(0,s);
-                fName = inputStr.substring (s+1);
+                lName = fullName.substring(0,s);
+                fName = fullName.substring (s+1);
                 String fullname = lName = fName;
             } 
         }
-        while(inputStr.indexOf(",")==-1){ // 3 or 4
-            if(inputStr.indexOf(" ")!= inputStr.lastIndexOf(" ")){
-                fName = inputStr.substring(0,s);        
-                mName = inputStr.substring (s+1,inputStr.lastIndexOf(" "));
-                lName = inputStr.substring(inputStr.lastIndexOf(" ")+1);
+        while(fullName.indexOf(",")==-1){ // 3 or 4
+            if(fullName.indexOf(" ")!= fullName.lastIndexOf(" ")){
+                fName = fullName.substring(0,s);        
+                mName = fullName.substring (s+1,fullName.lastIndexOf(" "));
+                lName = fullName.substring(fullName.lastIndexOf(" ")+1);
                 String fullName = fName + mName + lName;
             }else{
-                fName = inputStr.substring(0,s);
-                lName = inputStr.substring (s+1);
+                fName = fullName.substring(0,s);
+                lName = fullName.substring (s+1);
                 String fullName = fName + lName;
             }
-        
+
         }                               
 
-        return fullName;
-    
-    
+        //return fullName;
+
     }
 }
 
--- Scanner/ScannerTest/Runner.java
+++ Scanner/ScannerTest/Runner.java
@@ -9,12 +9,14 @@ import java.util.Scanner;
 public class Runner
 {
     public static void main(){
-        Scanner Person = new Scanner(System.in); 
+        Scanner kb = new Scanner(System.in); 
         String inputStr = "";
 
         while(!inputStr.equals("quit")){
             System.out.println("Enter a name or type \"quit\" to quit");
-            inputStr = Person.nextLine();
+            inputStr = kb.nextLine();
+            
+            Person p1 = new Person(inputStr);
             System.out.println(inputStr);
         }
         
