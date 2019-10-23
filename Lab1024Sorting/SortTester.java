11a0a86481c5464a3df730530819da94259d6e70
Kayka Garcia <kgarcia@WS-ACADBUS.EDU>
2019-10-23 10:13:36 -0700

    commit

--- Lab1024Sorting/SortTester.java
+++ Lab1024Sorting/SortTester.java
@@ -25,33 +25,45 @@ public class SortTester
                 if (numList.get(j) > numList.get(j+1)){
                     
                     Collections.swap (numList,j, j+1);
-                    System.out.println("Bubble sort: ");
-                    System.out.println(numList);
+                    
                 }
             }
         }
-
+        System.out.println("Bubble sort: ");
+                    System.out.println(numList);
     }
 
     public  void insertSort(){
         int temp;
         for(int i=1; 1<numList.size();i++){
-            for (int j = i; j >0 ; j --){
-                if(numList.get(j) < numList.get(j-1)){
-                    temp = numList.get(j);
-                    j = i;
-                    while ((j > 0) && (numList.get(j - 1) > temp)) {
-                        numList.set(j, numList.get(j - 1));
-                        j--;
-                    }
-                    numList.set(j, temp);
-                    
-                    System.out.println("Insertion Sort: ");
-                    System.out.println(numList);
-                }
+            int j=i;
+            while((j>0)&&numList.get(j)<numList.get(j-1)){
+                Collections.swap(numList,j,j-1);
+                j=j-1;
             }
         }
-    }        
+        System.out.println("Insertion Sort");
+        System.out.println(numList);
+    }      
+    
+    public void selectionSort(){
+        for(int i=0; i<numList.size()-1; i++){
+            int index =i;
+            for(int j= i+1; j<numList.size(); j++){
+                if(numList.get(j)<numList.get(index)){
+                    index = j;
+                }
+            }
+            int smallerNumber = numList.get(index);
+            numList.set(index,numList.get(i));
+            numList.set((i),smallerNumber);                      
+        }
+        System.out.println("Selection Sort");
+        System.out.println(numList);
+    }
+    
+    
+    
 }
 
     
