5317cd79d67e3d2161788d93245db1e23118a9cd
797067 <54371478+797067@users.noreply.github.com>
2020-02-26 10:19:11 -0800

    c

--- Lab1024Sorting/SortTester.java
+++ Lab1024Sorting/SortTester.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class SortTester here.
+ *.
  *
  * @author (Kayla Garcia)
  * @version (1024)
@@ -10,7 +10,7 @@ import java.util.*;
 public class SortTester
 {
     private  ArrayList<Integer> numList = new ArrayList<Integer>();
-
+    private int arr[];
     public  void loadArray(ArrayList<Integer> numList, int numOfElements){
         for(int i = 0; i < numOfElements; i++){
             numList.add((int)(Math.random()*100)+1);
@@ -23,14 +23,14 @@ public class SortTester
         for(int i = numList.size() -1; i>0; i--){
             for(int j =0; j<i;j++){
                 if (numList.get(j) > numList.get(j+1)){
-                    
+
                     Collections.swap (numList,j, j+1);
-                    
+
                 }
             }
         }
         System.out.println("Bubble sort: ");
-                    System.out.println(numList);
+        System.out.println(numList);
     }
 
     public  void insertSort(){
@@ -45,7 +45,7 @@ public class SortTester
         System.out.println("Insertion Sort");
         System.out.println(numList);
     }      
-    
+
     public void selectionSort(){
         for(int i=0; i<numList.size()-1; i++){
             int index =i;
@@ -61,18 +61,40 @@ public class SortTester
         System.out.println("Selection Sort");
         System.out.println(numList);
     }
-    
-    public static void printlist(String[]args){
-        
-    
-    
-    
+
+    public void mergeSort(int []arr, int n){
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = arr[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(arr, l, r, mid, n - mid);
     }
-    
+
+    public static void merge(int [] arr, int[]l, int []r, int left,int right){
+        int i=0,j=0,k=0;
+        while(i<left&&j<right){
+            if (l[i]<=r[j]){
+                arr[k++]=l[i++];
+            }else{
+                arr[k++] = r[j++];
+            }
+        }
+        while(i<left){
+            arr[k++] = l[i++];}
+        while(j<right){
+            arr[k++] = r[j++];}
+    }
 }
-
-    
-
-
-
 
